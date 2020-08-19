acbc62a71c9d2c9a3b1db5b97f1cc9dc189cc413
96jeferson <53370822+96jeferson@users.noreply.github.com>
2020-08-19 18:18:17 -0300

    Commit inicial

---
+++ aplicativo/src/test/java/io/github/jeferson/AplicativoApplication.java
@@ -0,0 +1,11 @@
+package io.github.96jeferson;
+
+import org.springframework.boot.SpringBootApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+public class AplicativoApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(AplicativoApplication.class, args);
+
+    }
+}
---
+++ venddas/src/main/java/io/github/costajeferson/VendasApplication.java
@@ -0,0 +1,2 @@
+package io.github.costajeferson;public class VendasApplication {
+}
